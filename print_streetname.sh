d74c2225138273d4faad137d93510fb230216398
trolleway <trolleway@yandex.ru>
2020-05-20 23:19:08 +0300

    upd

--- print_streetname.sh
+++ print_streetname.sh
@@ -11,10 +11,16 @@ village=$(curl -s "$url" | jq '.address.village'| tr -d '"'| tr -d 'null')
 
 text="$village\n$city\n$road"
 
-echo $road
+#echo $road
 
-filename=$(basename -s .JPG "$1")
-echo "$text" > "$filename".txt
-textfile="$filename".txt
-text=$(cat $textfile)
-convert $1 -pointsize 100 -fill white -undercolor gray -gravity SouthWest -annotate +266+235 "$text" texted/$1
+#filename=$(basename -s .JPG "$1")
+#echo "$text" > "$filename".txt
+#textfile="$filename".txt
+#text=$(cat $textfile)
+
+frame_path=$(dirname "$1")
+frame_name=$(basename -s .JPG "$1")
+frame_full_path=$frame_path/videoframes/$frame_name
+#mkdir -p $frame_path/texted
+#convert $1 -crop 4608x2592+0+0 -pointsize 100 -fill white -undercolor gray -gravity SouthWest -annotate +266+235 "$text" $frame_full_path.JPG
+convert $1 -crop 4608x2592+0+0 -pointsize 100 -fill white -undercolor gray -gravity NorthWest -annotate +266+2100 "$text" $frame_full_path.JPG
--- timelapse_streetname.sh
+++ timelapse_streetname.sh
@@ -1,8 +1,18 @@
+#!/bin/sh
 
+#mkdir $1/texted
+mkdir -p $1/videoframes
+mkdir -p $1/videoframes/texted
 
-mkdir texted
-mkdir videoframes
-ls -d *.JPG | parallel  convert -crop 4608x2592+0+0  "{}" "videoframes/{}"
+echo "1 (3)"
+#ls -d $1/*.JPG | parallel --bar convert -crop 4608x2592+0+0  "{}" "$1/videoframes/{/}"    # {/} - extract filename from path in parallel
 
-ls -d videoframes/*.JPG | parallel ./print_streetname.sh {}
+echo "2 (3)"
+ls -d $1/*.JPG |  parallel --bar ./print_streetname.sh {}
 
+echo "3 (3)"
+ffmpeg  -pattern_type glob -i "$1/videoframes/*.JPG" -c:v libx264 -preset faster -crf 22 -framerate 24 -c:a copy -y $1/out.mp4
+#rm -rf $1/videoframes
+#rm -rf $1/videoframes/texted
+
+echo $1/out.mp4
